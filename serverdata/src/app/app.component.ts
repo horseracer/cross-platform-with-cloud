c808b32baaca00dae15be62ac0102b4efd374480
Teerachai Laothong <tlaothong@hotmail.com>
2016-11-30 15:22:31 +0700

    Add TODO Page.

--- serverdata/src/app/app.component.ts
+++ serverdata/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { ListDataPage } from '../pages/list-data/list-data';
+import { TodoPage } from '../pages/todo/todo';
 
 
 @Component({
@@ -24,7 +25,8 @@ export class MyApp {
     this.pages = [
       { title: 'Page One', component: Page1 },
       { title: 'Page Two', component: Page2 },
-      { title: 'List Data', component: ListDataPage }
+      { title: 'List Data', component: ListDataPage },
+      { title: 'TODO', component: TodoPage }
     ];
 
   }
--- serverdata/src/app/app.module.ts
+++ serverdata/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { ListDataPage  } from '../pages/list-data/list-data';
+import { TodoPage } from '../pages/todo/todo';
 
 @NgModule({
   declarations: [
     MyApp,
     Page1,
     Page2,
-    ListDataPage
+    ListDataPage,
+    TodoPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,7 +22,8 @@ import { ListDataPage  } from '../pages/list-data/list-data';
     MyApp,
     Page1,
     Page2,
-    ListDataPage
+    ListDataPage,
+    TodoPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
